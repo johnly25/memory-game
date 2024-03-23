288efa8aa66a6b48cc08485bf1972193cef9cde0
johnny <94998830+johnly25@users.noreply.github.com>
2024-03-23 12:13:21 -0700

    restore previous version

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import '../styles/Cards.css'
 import shuffle from "./helpers";
 import { Card } from "./Card";
 
 export function Cards(props) {
     const [list, setList] = useState([]);
-    const [isFlipped, setIsFlipped] = useState(false);
-
+    const [totalCards, setTotalCards] = useState(0);
 
     useEffect(() => {
+        console.log('fetch');
         const url = 'https://dragonball-api.com/api/characters?race=Saiyan&affiliation=Z%20fighter';
+
         async function fetchDBZ(url) {
             try {
                 const response = await fetch(url);
@@ -17,20 +19,24 @@ export function Cards(props) {
                 const jsonSliced = json.slice(0, 9);
                 const data = shuffle(jsonSliced);
                 setList(data);
-                props.setGame({ ...props.game, totalCards: data.length });
+                props.setGame({...props.game, totalCards: data.length});
             } catch (error) {
                 console.log(error);
             }
         }
-        fetchDBZ(url);
+        fetchDBZ(url)
     }, [props.game.gameList]);
 
+    useEffect(() => {
+        console.log(props.game)
+        if (props.game.score == totalCards && props.game.score != 0) {
+            console.log('game over cards');
+            props.setGame({ ...props.game, gameOver: true, playerWin: true });
+        }
+    }, [props.game.score]);
 
-
-    
-
-    const cards = list.map((card, i) =>
-        <Card {...props} key={i} src={card.image} isFlipped={isFlipped} setIsFlipped={setIsFlipped}/>
+    const cards = list.map(card =>
+        <Card {...props} key={card.id} src={card.image} />
     );
 
     return (
